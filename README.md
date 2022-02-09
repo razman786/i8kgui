63fc4727f99a706e5217881923e044a9bbe0ea9b
Raz <razman786@gmail.com>
2022-02-09 15:20:18 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # i8kgui
-A system tray GUI for i8kutils
+A system tray GUI for i8kutils.
+
+## Prerequisites 
+
+### i8kutils
+
+Please install and configure i8kutils from https://github.com/vitorafsr/i8kutils.  
+
+## Installation
+
+`python3 setup.py install --user`
+
+## Issues and requests
+
+Please open an issue here https://github.com/razman786/i8kutils/issues.
+
+## Credits
+
+Delapouite <https://delapouite.com/> for the Computer Fan Icon (CC BY 3.0 <http://creativecommons.org/licenses/by/3.0/>)
+
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+__version__ = "0.1"
+
+requirements = [
+    'PySide6',
+]
+
+scripts = [
+    'i8kgui',
+]
+
+package_data = {'i8kgui': ['icons/*.png']}
+
+setup(
+    name='i8kgui',
+    version=__version__,
+    packages=find_packages(),
+    scripts=scripts,
+    package_data=package_data,
+    install_requires=requirements,
+    url='https://github.com/razman786/i8kgui',
+    license='GPL',
+    author='Dr Rahim Lakhoo',
+    author_email='razman786@gmail.com',
+    description='A system tray GUI for i8kutils.',
+    data_files=[
+        (f"{Path.home()}/.local/share/applications", ['desktop/i8kgui.desktop']),
+        (f"{Path.home()}/.local/share/icons", ['icons/i8kgui_icon.png']),
+    ],
+)
