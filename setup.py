695739e66678bdfcd30fa21b132e8408b016be58
Raz <razman786@gmail.com>
2022-02-13 23:17:56 +0000

    Updated setup.py to handle system install. Restructured project files.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages
+import os
 from pathlib import Path
+
+from setuptools import setup, find_packages
 
 __version__ = "0.4"
 
@@ -9,7 +11,23 @@ requirements = [
 
 scripts = [
     'i8kgui/i8kgui',
+    'i8kgui/i8kgui_thermal_control',
 ]
+
+
+def get_data_files():
+    if os.geteuid() == 0:
+        files = [
+            (f"/usr/share/applications", ['i8kgui/desktop/i8kgui.desktop']),
+            (f"/usr/share/icons", ['i8kgui/icons/i8kgui_icon.png']),
+        ]
+    else:
+        files = [
+            (f"{Path.home()}/.local/share/applications", ['i8kgui/desktop/i8kgui.desktop']),
+            (f"{Path.home()}/.local/share/icons", ['i8kgui/icons/i8kgui_icon.png']),
+        ]
+    return files
+
 
 setup(
     name='i8kgui',
@@ -22,8 +40,5 @@ setup(
     author='Dr Rahim Lakhoo',
     author_email='razman786@gmail.com',
     description='A system tray GUI for i8kutils and smbios.',
-    data_files=[
-        (f"{Path.home()}/.local/share/applications", ['i8kgui/desktop/i8kgui.desktop']),
-        (f"{Path.home()}/.local/share/icons", ['i8kgui/icons/i8kgui_icon.png']),
-    ],
+    data_files=get_data_files(),
 )
