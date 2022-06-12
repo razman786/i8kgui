b163572410265c90fa7899a0c366869c72913df5
Raz <razman786@gmail.com>
2022-06-12 13:07:23 +0100

    Updated setup.py to install polkit action policies when installed with sudo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import find_packages, setup
 
-__version__ = "0.5"
+__version__ = "0.6"
 
 requirements = [
     'PySide6',
@@ -16,20 +16,20 @@ scripts = [
 
 def get_data_files():
     return [
-            ("/usr/share/applications", ['i8kgui/desktop/i8kgui.desktop']),
-            ("/usr/share/icons", ['i8kgui/icons/i8kgui_icon.png']),
-        ] if os.geteuid() == 0 else [
-            ("{Path.home()}/.local/share/applications",
-             ['i8kgui/desktop/i8kgui.desktop']),
-            ("{Path.home()}/.local/share/icons",
-             ['i8kgui/icons/i8kgui_icon.png']),
-        ]
+        ("/usr/share/applications", ['i8kgui/desktop/i8kgui.desktop']),
+        ("/usr/share/icons", ['i8kgui/icons/i8kgui_icon.png']),
+        ("/usr/share/polkit-1/actions", ['i8kgui/polkit_actions/com.ubuntu.pkexec.i8kgui_thermal_control.policy']),
+        ("/usr/share/polkit-1/actions", ['i8kgui/polkit_actions/com.ubuntu.pkexec.smbios-thermal-ctl.policy']),
+    ] if os.geteuid() == 0 else [
+        ("{Path.home()}/.local/share/applications", ['i8kgui/desktop/i8kgui.desktop']),
+        ("{Path.home()}/.local/share/icons", ['i8kgui/icons/i8kgui_icon.png']),
+    ]
 
 
 setup(
     name='i8kgui',
     version=__version__,
-    packages=find_packages(),
+    packages=['i8kgui'],
     scripts=scripts,
     install_requires=requirements,
     url='https://github.com/razman786/i8kgui',
